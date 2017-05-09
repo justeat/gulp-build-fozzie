11efe75b93c4fb3eab84adb16bb17204b0272a40
Damian Mullins <damian.mullins@just-eat.com>
2017-05-09 15:36:14 +0100

    Separated watch tasks out into individual tasks.

--- tasks/watch.js
+++ tasks/watch.js
@@ -4,20 +4,38 @@ const config = require('../config');
 /**
  * watch Task
  * -------------
- * Watches for changes to JavaScript, CSS, and image file changes and
- * runs the relevant build tasks for each type.
+ * Watches for changes to JavaScript, CSS, and image file changes, running
+ * relevant build tasks on change for each type.
  *
  */
-gulp.task('watch', ['default'], () => {
+gulp.task('watch', ['default', 'watch:scripts', 'watch:scripts:test', 'watch:css', 'watch:images']);
+
+
+gulp.task('watch:scripts', () => {
 
     gulp.watch(`${config.js.srcDir}/**/!(*.test).js`, ['scripts'])
         .on('change', config.gulp.changeEvent);
 
+});
+
+
+gulp.task('watch:scripts:test', () => {
+
     gulp.watch(`${config.js.srcDir}/**/*.test.js`, ['scripts:lint', 'scripts:test'])
         .on('change', config.gulp.changeEvent);
 
+});
+
+
+gulp.task('watch:css', () => {
+
     gulp.watch(`${config.css.srcDir}/**/*.scss`, ['css'])
         .on('change', config.gulp.changeEvent);
+
+});
+
+
+gulp.task('watch:images', () => {
 
     gulp.watch(`${config.img.srcDir}/**/*.{png,jpg,jpeg,gif,svg}`, ['images'])
         .on('change', config.gulp.changeEvent);
