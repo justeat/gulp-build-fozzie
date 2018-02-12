1d0e15fd44285dcfc50e42dfab8799a24fa55715
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2018-02-12 16:46:46 +0000

    CPD-1653: Removing console logs and fixing global require statement.

--- tasks/logger-file.js
+++ tasks/logger-file.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const pathBuilder = require('../pathBuilder');
 const config = require('../config');
+const fs = require('fs');
 
 /**
  *  `logger-file-create` Task
@@ -9,14 +10,10 @@ const config = require('../config');
  */
 
 gulp.task('logger-file-create', () => {
-    const fs = require('fs');
     const fullFilePath = `${pathBuilder.jsErrorLoggerSubDir}/${config.logger.file}`;
     if (!fs.existsSync(fullFilePath)) {
         fs.mkdirSync(`${pathBuilder.jsErrorLoggerDir}`);
         fs.mkdirSync(`${pathBuilder.jsErrorLoggerSubDir}`);
         fs.writeFileSync(fullFilePath, '//empty file');
-        console.log('Error logging endpoint file added to the filesystem');
-    } else {
-        console.log('Error logging endpoint file already exists in the filesystem');
     }
 });
